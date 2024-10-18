ed52d30688892485dd2c6c2f41d423d39dbf9b81
skociuba <skociuba83@gmail.com>
2024-10-18 13:52:04 +0200

    fix

--- src/app/[locale]/components/templates/MainLayout/MainLayout.tsx
+++ src/app/[locale]/components/templates/MainLayout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { ReactNode } from 'react';
 
-import { cn } from '@/lib/utils';
+import { cn } from './../../../lib/utils/cn';
 
 interface MainLayoutProps {
   children: ReactNode;
